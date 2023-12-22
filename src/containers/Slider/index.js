588d03f8e800bf316c816b1e3fdb2fba7b550ec5
Clemence Vidal <clemencevidal0512@gmail.com>
2023-12-22 13:48:23 +0100

    Résolution de tous les (sauf 1) problèmes de console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,10 +11,12 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
-      5000
-    );
+    if(byDateDesc && byDateDesc.length > 0) {
+      setTimeout(
+        () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
+        5000
+      );
+    }    
   };
   useEffect(() => {
     nextCard();
@@ -42,7 +44,7 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${_.title}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
